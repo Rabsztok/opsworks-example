7de598da1cafc9216e93bba1ce4a99e1e148d1f3
rabsztok <rabsztok@gmail.com>
2013-09-19 12:10:09 +0200

    Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpsworksExample::Application.config.secret_key_base = '567c89e0473fe8bcef589543e84d737fe920cf1428675f26816a7ca1c56d26567341c13fcc51f09a71a3bdcd9b9dc01419b99c8cc08cae739f9e8c2dac54ff0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpsworksExample::Application.config.session_store :cookie_store, key: '_opsworks-example_session'
